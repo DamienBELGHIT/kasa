f366ec2f78b5bc0ba7fdd75ca148c3f37fe99d16
Damien BELGHIT <damien.belghit@yahoo.fr>
2023-01-27 16:04:48 +0100

    House page Slideshow

---
+++ src/components/Slideshow.jsx
@@ -0,0 +1,38 @@
+import "../styles/slideshow.css"
+import { useState } from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons"
+
+function Slideshow({ images }) {
+  const [imageIndex, setimageIndex] = useState(0)
+  return (
+    <div className="slideshow">
+      {images.length > 1 && (
+        <button
+          className="arrow-btn left"
+          onClick={() =>
+            setimageIndex(imageIndex > 0 ? imageIndex - 1 : images.length - 1)
+          }
+        >
+          <FontAwesomeIcon icon={faChevronLeft} />
+        </button>
+      )}
+
+      <img src={images[imageIndex]} alt={images[imageIndex]} />
+
+      {images.length > 1 && (
+        <button
+          className="arrow-btn right"
+          onClick={() => setimageIndex((imageIndex + 1) % images.length)}
+        >
+          <FontAwesomeIcon icon={faChevronRight} />
+        </button>
+      )}
+    </div>
+  )
+}
+
+export default Slideshow
--- src/pages/House.jsx
+++ src/pages/House.jsx
@@ -1,9 +1,15 @@
 import { useParams } from "react-router-dom"
+import Slideshow from "../components/Slideshow"
+import housesData from "../data/houses.json"
+
+const housesList = JSON.parse(JSON.stringify(housesData))
 
 function House() {
   const { houseID } = useParams()
+  const house = housesList.find((houseElem) => houseElem.id === houseID)
   return (
     <main>
+      <Slideshow images={house.pictures} />
       <div>House {houseID}</div>
     </main>
   )
