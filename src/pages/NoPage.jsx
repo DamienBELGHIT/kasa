c8c4b2ecf4bd7c28e591a532d5c02f8c6d6c6c5c
Damien BELGHIT <damien.belghit@yahoo.fr>
2023-01-27 15:03:53 +0100

    404 Error page complete

--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom"
+import "../styles/nopage.css"
+
 const NoPage = () => {
   return (
     <main>
-      <h1>404</h1>
+      <h1 className="error-title">404</h1>
+      <h2 className="error-subtitle">
+        Oups! La page que vous demandez n'existe pas.
+      </h2>
+      <Link className="error-link" to="/">
+        Retourner sur la page d'accueil
+      </Link>
     </main>
   )
 }
