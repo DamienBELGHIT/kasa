93187884dea4685c6270bbe0f2c21ab85b2053fa
Damien BELGHIT <damien.belghit@yahoo.fr>
2023-01-30 11:15:09 +0100

    Error on wrong parameter redirect

--- src/pages/House.jsx
+++ src/pages/House.jsx
@@ -3,6 +3,7 @@ import Slideshow from "../components/Slideshow"
 import housesData from "../data/houses.json"
 import Collapse from "../components/Collapse"
 import Rating from "../components/Rating"
+import { Navigate } from "react-router-dom"
 import "../styles/house.css"
 
 const housesList = JSON.parse(JSON.stringify(housesData))
@@ -10,50 +11,55 @@ const housesList = JSON.parse(JSON.stringify(housesData))
 function House() {
   const { houseID } = useParams()
   const house = housesList.find((houseElem) => houseElem.id === houseID)
-
   return (
     <main className="house-main">
-      <Slideshow images={house.pictures} />
-
-      <div className="house-infos">
+      {!house ? (
+        <Navigate to="/error" replace={true} />
+      ) : (
         <div>
-          <h1>{house.title}</h1>
-          <h2>{house.location}</h2>
+          <Slideshow images={house.pictures} />
 
-          <div className="house-tags">
-            {house.tags.map((tag, index) => (
-              <div key={index} className="tag">
-                {tag}
+          <div className="house-infos">
+            <div>
+              <h1>{house.title}</h1>
+              <h2>{house.location}</h2>
+
+              <div className="house-tags">
+                {house.tags.map((tag, index) => (
+                  <div key={index} className="tag">
+                    {tag}
+                  </div>
+                ))}
               </div>
-            ))}
-          </div>
-        </div>
+            </div>
 
-        <div className="house-profile">
-          <div className="house-host">
-            <h2>{house.host.name}</h2>
-            <div className="host-picture">
-              <img src={house.host.picture} alt={house.host.name} />
+            <div className="house-profile">
+              <div className="house-host">
+                <h2>{house.host.name}</h2>
+                <div className="host-picture">
+                  <img src={house.host.picture} alt={house.host.name} />
+                </div>
+              </div>
+
+              <Rating rating={house.rating} />
             </div>
           </div>
 
-          <Rating rating={house.rating} />
+          <div className="house-collapses">
+            <Collapse title="Description" content={house.description} />
+            <Collapse
+              title="Équipement"
+              content={
+                <ul>
+                  {house.equipments.map((equipment, i) => (
+                    <li key={i}>{equipment}</li>
+                  ))}
+                </ul>
+              }
+            />
+          </div>
         </div>
-      </div>
-
-      <div className="house-collapses">
-        <Collapse title="Description" content={house.description} />
-        <Collapse
-          title="Équipement"
-          content={
-            <ul>
-              {house.equipments.map((equipment, i) => (
-                <li key={i}>{equipment}</li>
-              ))}
-            </ul>
-          }
-        />
-      </div>
+      )}
     </main>
   )
 }
